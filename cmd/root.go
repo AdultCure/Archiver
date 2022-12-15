8dccf49b2761eb874765c77753939248dee430bd
Artem Eremenko <artemeremenko.m@yandex.ru>
2022-12-15 17:58:38 +0300

    Initial commit

---
+++ cmd/root.go
@@ -0,0 +1,24 @@
+package cmd
+
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+	"os"
+)
+
+var rootCmd = &cobra.Command{
+	Short: "Simple archiver",
+}
+
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		handleErr(err)
+	}
+}
+
+func handleErr(err error) {
+	if err := rootCmd.Execute(); err != nil {
+		_, _ = fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
